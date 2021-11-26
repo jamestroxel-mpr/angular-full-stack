c0dbc921cc4d1c9f014c145fcbc233addd374228
James Troxel <JTroxel@mathematica-mpr.com>
2021-11-26 12:13:36 -0500

    first commit

---
+++ buy-and-sell-backend/src/routes/getLIstings.js
@@ -0,0 +1,14 @@
+import { fakeListings } from "./fake-data";
+import Boom from "@hapi/boom";
+
+export const getListingRoute = {
+    method: 'GET',
+    path: '/api/listings/{id}',
+    handler: (req, h) => {
+        const id = req.params.id;
+        const listing = fakeListings.find(listing => listing.id === id)
+        if (!listing) throw Boom.notFound('Listing does not exist');
+        return listing;
+    }
+
+}
---
+++ buy-and-sell-backend/src/routes/index.js
@@ -0,0 +1,6 @@
+import { getAllLIstingsRoute } from "./getAllLIstings";
+import { getListingRoute } from "./getLIstings";
+export default [
+    getAllLIstingsRoute,
+    getListingRoute
+];
